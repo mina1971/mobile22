8e22afba263b81fa5a2c283f5a930f096afa7599
mina1971 <jommina62@gmail.com>
2021-03-19 21:23:20 +0200

    room commit

---
+++ js/js.js
@@ -0,0 +1,12 @@
+// ‫ إلغاء الحركة في شاشات الجوال وتهيئة مكتبة wow للعمل
+// تلغى التأثيرات فقط فى النسخة اقل من 4 فى مقاس الجوال
+$(document).ready(function(){
+var wow = new WOW({
+    mobile: false
+});
+wow.init();
+
+// emailForm على النموذج validate تطبيق التابع
+$(function () {
+    $("#emailForm").validate();
+ });
